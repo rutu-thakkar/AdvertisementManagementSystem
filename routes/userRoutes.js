da809e6097c3907cf5a2e89fdd90eb085a324e3f
Rutu Thakkar <ruhithakkar10@gmail.com>
2022-01-24 18:44:39 +0530

    Email link verificarion and delete user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const userController = require('../controller/userController');
-const multer = require('multer');
+const userController = require("../controller/userController");
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + file.originalname + "-" + Date.now() + ".jpg");
-    }
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + file.originalname + "-" + Date.now() + ".jpg");
+  },
 });
 
 var upload = multer({ storage: storage });
 
-route.get('/', userController.home);
-route.get('/getAllUsers', userController.getAllUsers)
-route.post('/signup', upload.single('profile'), userController.signup);
-route.post('/login', userController.login);
+route.get("/", userController.home);
+route.get("/getAllUsers", userController.getAllUsers);
+route.post("/signup", upload.single("profile"), userController.signup);
+route.get("/:token", userController.verify);
+route.post("/login", userController.login);
+route.delete("/deleteUser", userController.deleteUser);
 // route.put('/updateUser', userController.updateUser);
 
 module.exports = route;
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const YAML = require('yamljs');
-const swaggerUI = require('swagger-ui-express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const YAML = require("yamljs");
+const swaggerUI = require("swagger-ui-express");
 const app = express();
-const userRoutes = require('./routes/userRoutes');
-const db = require('./models');
+const userRoutes = require("./routes/userRoutes");
+const db = require("./models");
 const port = process.env.PORT || 3000;
-const JSDoc = YAML.load('./api.yaml');
+const JSDoc = YAML.load("./api.yaml");
 
 app.use(cors());
 app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(JSDoc))
+app.use("/users", userRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(JSDoc));
 
-db.users.sync( {alter : true} ).then(() => {
+db.users
+  .sync()
+  .then(() => {
     app.listen(port, () => {
-        console.log(`listening on http://localhost:${port}`);
-    })
-}).catch(err => {
+      console.log(`listening on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
     console.error(err);
-})
+  });
